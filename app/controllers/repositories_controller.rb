63f22dc0e4bf6f5ee79455092cf51809240b460a
Pavel Gorgolevskiy <gorgolevsky@gmail.com>
2020-05-16 01:11:01 +0300

    1 Implemented the most basic skeleton for RepositoriesController#index action 2 Disabled javascript for now

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,28 @@
 class RepositoriesController < ApplicationController
+  # TODO: Make Ajax request to refresh list of repositories.
   def index
-    # params[:query]
+    if params[:query]
+      process_repositories_query
+    else
+      @repositories = []
+    end
+  end
+
+  private
+
+  def process_repositories_query
+    res = Github::Client::RepositoriesSearch.new(query: params[:query]).call
+
+    if res.success?
+      @repositories = JSON.parse(res.body)['items'].map do |el|
+        # TODO: move this to a presenter:
+        el.slice('full_name', 'html_url', 'description')
+      end
+    else
+      # TODO: add more details here of course:
+      # TODO: return non-200 http code on non-successful github request
+      flash.now[:alert] = "Something went wrong"
+      @repositories = []
+    end
   end
 end
