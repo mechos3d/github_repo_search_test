f8df1c845b4734b5d8fc824e5d09656b3727e1f1
Pavel Gorgolevskiy <gorgolevsky@gmail.com>
2020-05-16 15:19:36 +0300

    Returned taking of gitlba auth token from ENV in settings.yml. It didn't work the first time because of Spring caching something

--- lib/github/client.rb
+++ lib/github/client.rb
@@ -2,10 +2,7 @@
 
 module Github
   class Client
-    BASE_URL = 'https://api.github.com/' # TODO: can take this from application settings
-
-    # TODO: Although 'Settings' (gem 'config') supports ERB in it -
-    # for some reason it cannot get ENV-variables here, hence this crutch:
-    AUTHORIZATION = Settings.github.client.authorization || ENV.fetch('GITHUB_AUTHORIZATION')
+    AUTHORIZATION = Settings.github.client.authorization
+    BASE_URL = 'https://api.github.com/'
   end
 end
